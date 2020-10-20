7bc4e0d8f3d89ea1f42d2f58e854d50f0115f203
Brian Moriguchi <brian@beckos.com>
2020-10-19 20:34:33 -0700

    Initial Commit

---
+++ iBottaTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iBottaTest
+//
+//  Created by Tatsuya Moriguchi on 10/19/20.
+//  Copyright © 2020 Tatsuya Moriguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
