e1264da2b99d54469a13424ddf30bbd2257ec2cc
Brian Moriguchi <brian@beckos.com>
2020-10-19 20:42:47 -0700

    UICollectionView with no storyboard

---
+++ iBottaTest/File.swift
@@ -0,0 +1,35 @@
+//
+//  File.swift
+//  iBottaTest
+//
+//  Created by Tatsuya Moriguchi on 10/19/20.
+//  Copyright © 2020 Tatsuya Moriguchi. All rights reserved.
+//
+
+import UIKit
+
+class ColorConvert {
+    func hexStringToUIColor (hex:String, alphaValue: Double) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+
+        var rgbValue:UInt64 = 0
+        //Scanner(string: cString).scanHexInt32(&rgbValue)
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(alphaValue)
+        )
+    }
+}
--- iBottaTest/ViewController.swift
+++ iBottaTest/ViewController.swift
@@ -8,13 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let cellID = "cellID"
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            collectionView?.backgroundColor = ColorConvert().hexStringToUIColor(hex: "FFFFFF", alphaValue: 1.0)
+            navigationItem.title = "Teams"
+            navigationController?.navigationBar.barTintColor = ColorConvert().hexStringToUIColor(hex: "ABFCD6", alphaValue: 1.0)
+            navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white, NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 20)]
+            
+            collectionView?.register(TeamCell.self, forCellWithReuseIdentifier: cellID)
+            
+        
+            
+        }
+        
+        override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return 6
+        }
+        
+        override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as! TeamCell
+            return cell
+        }
+
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            return CGSize(width: (view.frame.width / 2) - 16, height: 100)
+        }
+            
+        
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,  insetForSectionAt section: Int) -> UIEdgeInsets {
+            return UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+
+        }
+
+
     }
 
 
-}
+    class TeamCell: UICollectionViewCell {
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            setup()
+        }
+        
+        func setup() {
+            self.backgroundColor = ColorConvert().hexStringToUIColor(hex: "4A4A4A", alphaValue: 1.0)
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not implemented.")
+        }
+    }
+
+
+
 
